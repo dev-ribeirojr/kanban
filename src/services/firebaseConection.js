de936429285d4da380f50eca88d0024cb0372465
ribeiro-jr <ribeirojr1522@gmail.com>
2023-08-26 16:07:50 -0300

    :loock: Colocando segurança utilizando variáveis de hambiente

--- src/services/firebaseConection.js
+++ src/services/firebaseConection.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBS5zSbUJrdyERFfaLxA6YPQ04kF5s2X-s",
-  authDomain: "gerenciador-de-tarefas-6a07b.firebaseapp.com",
-  projectId: "gerenciador-de-tarefas-6a07b",
-  storageBucket: "gerenciador-de-tarefas-6a07b.appspot.com",
-  messagingSenderId: "577758994164",
-  appId: "1:577758994164:web:3a6ac45ce990d90d3f48a1",
-  measurementId: "G-ZC8BRGQDKJ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASEREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
